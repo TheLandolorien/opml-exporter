904a3d3caf439cf5cd23113092c7386866dee6c7
Lando Toothman <25360446+TheLandolorien@users.noreply.github.com>
2023-07-14 16:39:24 -0400

    feat: Connect to local Apple Podcasts database

--- src/opml_exporter/cli.py
+++ src/opml_exporter/cli.py
@@ -1,2 +1,18 @@
+import glob
+import os
+import sqlite3
+
+MACOS_USER_PODCAST_PATH = (
+    "~/Library/Group Containers/*.groups.com.apple.podcasts/Documents/MTLibrary.sqlite"
+)
+
+
 def run() -> None:
-    raise NotImplementedError("opml-exporter is not Implemented")
+    try:
+        (db_path,) = glob.glob(os.path.expanduser(MACOS_USER_PODCAST_PATH))
+    except ValueError as err:
+        raise FileNotFoundError("ERROR: No podcast database found") from err
+
+    sql_manager = sqlite3.connect(db_path, uri=True)
+
+    sql_manager.close()
--- tests/opml_exporter/test_cli.py
+++ tests/opml_exporter/test_cli.py
@@ -1,9 +1,23 @@
+from unittest.mock import patch
 import pytest
 
 from opml_exporter import cli
 
 
-def test_run_should_raise_not_implemented_error():
-    with pytest.raises(NotImplementedError):
+@patch("opml_exporter.cli.glob")
+def test_run_should_raise_if_missing_podcast_db(mock_glob):
+    mock_glob.glob.return_value = "foo"
+
+    with pytest.raises(FileNotFoundError) as err:
         cli.run()
-        assert False, "should raise NotImplementedError"
+        assert False, "should raise FileNotFoundError"
+
+    assert str(err.value) == "ERROR: No podcast database found"
+
+
+@patch("opml_exporter.cli.sqlite3")
+def test_run_should_query_local_apple_podcast_database(mock_sqlite3):
+    cli.run()
+
+    mock_sqlite3.connect.assert_called()
+    mock_sqlite3.connect.return_value.close.assert_called()
